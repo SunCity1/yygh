460961a47a96d0b51c45b84fa312ad50479f3760
LeCun <2899610146@qq.com>
2022-05-10 17:38:25 +0800

    项目初始环境搭建

---
+++ src/main/java/com/example/yygh/YyghApplication.java
@@ -0,0 +1,13 @@
+package com.example.yygh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyghApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YyghApplication.class, args);
+    }
+
+}
