554677ced6fc949c7aad01b2c70a745885de6b3e
LeCun <2899610146@qq.com>
2022-05-19 22:52:12 +0800

    上传医院接口初步开发

---
+++ service/service_hosp/src/main/java/com/example/yygh/hosp/controller/api/ApiController.java
@@ -0,0 +1,33 @@
+package com.example.yygh.hosp.controller.api;
+
+import com.example.yygh.common.helper.HttpRequestHelper;
+import com.example.yygh.common.result.Result;
+import com.example.yygh.hosp.service.HospitalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/hosp")
+public class ApiController {
+
+    @Autowired
+    private HospitalService hospitalService;
+
+    // 上传医院接口
+    @PostMapping("saveHospital")
+    public Result saveHosp(HttpServletRequest request) {
+        // 获取传递过来的医院信息,map接收
+        Map<String, String[]> requestMap = request.getParameterMap();
+        Map<String, Object> paramMap = HttpRequestHelper.switchMap(requestMap);
+
+        // 调用service 的方法
+        hospitalService.save(paramMap);
+        return Result.ok();
+    }
+
+}
